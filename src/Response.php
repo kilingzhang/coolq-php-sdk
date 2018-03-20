cd93386eb8e88001ec77e359e62d10d7f273b311
崎 <slight@kilingzhang.com>
2018-03-20 23:31:15 +0800

    coolq-php-sdk
    基本方封装完毕

---
+++ src/Response.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kilingzhang
+ * Date: 18-3-20
+ * Time: 下午7:29
+ */
+
+namespace CoolQSDK;
+
+
+class Response
+{
+
+}
---
+++ src/URL.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kilingzhang
+ * Date: 18-3-19
+ * Time: 下午11:50
+ */
+
+namespace CoolQSDK;
+
+
+class URL
+{
+
+}
