475c8c69aa64d9048a3833a49b450a2955eaa1a2
崎 <slight@kilingzhang.com>
2018-08-28 01:57:59 +0800

    :hear_no_evil: 测试样例
    更新phpunit 、添加测试样例

--- tests/GuzzleProtocolTest.php
+++ tests/GuzzleProtocolTest.php
@@ -2,26 +2,27 @@
 /**
  * Created by PhpStorm.
  * User: kilingzhang
- * Date: 2018/8/28
- * Time: 1:34
+ * Date: 2018/8/27
+ * Time: 21:41
  */
 
 namespace Kilingzhang\Tests;
 
+
 use Faker\Factory;
 use Faker\Generator;
-use Kilingzhang\QQ\CoolQ\QQ;
+use Kilingzhang\QQ\CoolQ\Url;
 use Kilingzhang\QQ\Core\Protocols\GuzzleProtocol;
 use Kilingzhang\QQ\Core\Response;
 use PHPUnit\Framework\TestCase;
 
-class CoolQTest extends TestCase
+class GuzzleProtocolTest extends TestCase
 {
 
     /**
-     * @var \Kilingzhang\QQ\Core\QQ
+     * @var GuzzleProtocol
      */
-    private $QQ;
+    private $protocol;
     /**
      * @var Generator
      */
@@ -33,14 +34,20 @@ class CoolQTest extends TestCase
 
     public function setUp()
     {
-        $this->QQ = new QQ(new  GuzzleProtocol($this->url, $this->accessToken, $this->secret));
+        $this->protocol = new GuzzleProtocol($this->url, $this->accessToken, $this->secret);
         $this->faker = Factory::create();
     }
 
-    public function testSendPrivateMsg()
+    public function testSend()
     {
-        $text = $this->faker->text;
-        $response = $this->QQ->sendPrivateMsg($this->devQQ, $text, false);
+        $message = $this->faker->text;
+        $response = $this->protocol->send(Url::send_private_msg, [
+            'user_id' => $this->devQQ,
+            'message' => $message,
+            'auto_escape' => false,
+        ]);
+
         $this->assertInstanceOf(Response::class, $response);
     }
+
 }
